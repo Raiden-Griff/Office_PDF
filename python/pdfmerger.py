58b3a7866212931e72643b727de39b9e8756fbb6
blood-orange-nocturne <evenightbringer@gmail.com>
2026-04-18 17:48:05 -0700

    Designed pdfmerger, works for as many pdfs as you want to merge, you sicko

--- python/pdfmerger.py
+++ python/pdfmerger.py
@@ -1,21 +1,43 @@
 from pypdf import PdfReader, PdfWriter
 
-def PopulatePdfs(pdfFilePathA : str, pdfFilePathB : str) :
-    global pdfA
-    global pdfB
-    pdfA = PdfReader(pdfFilePathA)
-    pdfB = PdfReader(pdfFilePathB)
+global pdfs
+global mergedPdfs
+pdfs = [PdfReader("default.pdf")]
+mergedPdfs = PdfWriter()
 
-def GetPageCountA() :
-    return pdfA.get_num_pages
-
-def GetPageCountB() :
-    return pdfB.get_num_pages
-
-def MergePdfs() :
-    global mergedPdfs
+# Requirement: list of strings to filepaths
+# Code must have access to such a path, and the files must be there
+# Resets pdfs and mergedPdfs
+def PopulatePdfs(pdfFilePaths : list[str]) :
     mergedPdfs = PdfWriter()
-    
+    pdfs.clear
+    for filePath in pdfFilePaths:
+        pdfs.append(PdfReader(filePath))
 
+# Requirement: index of the pdf desired
+# The order they were inserted should be known by you
+def GetPageCount(indexOf : int) :
+    return pdfs[indexOf].get_num_pages
+
+# For convenience, creates a "merged.pdf" written with the contents of the mergedPdf
+def WriteToFile() :
     with open("merged.pdf", "wb") as f:
         mergedPdfs.write(f)
+
+# Requirements: same number of values in pdfIndexes and pagenumbers
+# The pdf at index n should correlate with the page number at index n
+# Always creates a file, even in error conditions
+# PDF will just be empty (i think)
+def MergePdfs(pdfIndexes : list[int], pageNumbers : list[int]) :
+    if(len(pdfIndexes) != len(pageNumbers)) :
+        WriteToFile()
+        return
+    
+    if(len(pdfIndexes) == 0) :
+        WriteToFile()
+        return
+
+    for iterator in range(len(pdfIndexes)) :
+        mergedPdfs.add_page(pdfs[iterator].get_page(pageNumbers[iterator]))
+
+    WriteToFile()
